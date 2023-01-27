150b8b134380721ccfbdbf2c3ed614ed7928eab4
Shane OHanlon <sohanlon07@gmail.com>
2023-01-27 23:11:02 +0000

    Updated main.tf
    Added ELastic Load balancer
    Added ELB Security Group
    Changed output variable to DNS output

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ variable "server_port" {
     default = 8080 
 }
 resource "aws_launch_configuration" "example" {
-    ami = "ami-0fb653ca2d3203ac1"
+    image_id = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -37,8 +37,12 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
     launch_configuration = aws_launch_configuration.example.name
     vpc_zone_identifier = data.aws_subnets.default.ids
+
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
     min_size = 2
-    maxmax_size = 10
+    max_size = 10
 
     tag {
         key = "Name"
@@ -58,7 +62,80 @@ resource "aws_security_group" "instance" {
     }
 }
 
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnets.default.ids
+    security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+
+    # by default return a simple 404 page
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "404: Page not found"
+        status_code = 404
+      }
+    }
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+      path = "/"
+      protocol = "HTTP"
+      matcher = "200"
+      interval = 15
+      timeout = 3
+      healthy_threshold = 2
+      unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+
+    condition {
+      path_pattern {
+        values = ["*"]
+      }
+    }
+
+    action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.asg.arn
+    }
+}
 output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
 }
