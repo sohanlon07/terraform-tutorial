a12036dcf5e43f971183a420ea63e20a2b2d88e8
Shane OHanlon <sohanlon07@gmail.com>
2023-01-27 22:09:50 +0000

    Updated main.tf
    Added security group
    Added input variable for port
    Add public ip output

--- main.tf
+++ main.tf
@@ -2,12 +2,35 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+variable "server_port" {
+    description = "The port the server will use for http requests"
+    type = number
+    default = 8080 
+}
+resource "aws_launch_configuration" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
-    tags = {
-      "Name" = "terraform-example"
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
-  
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IP address of the web server"
 }
