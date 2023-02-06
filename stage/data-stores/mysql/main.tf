ec5e156906e23795c16032e0d4036d46f40660b0
Shane <sohanlon07@gmail.com>
2023-02-06 16:25:11 +0000

    Initial Commit
    Add Terraform files
    Add .gitignore

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-file-storage-sohan"
+    key  = "stage/datastores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-state-file-storage-sohan-locks"
+    encrypt = true
+  }
+
+}
+
+resource "aws_db_instance" "name" {
+    identifier_prefix = "terraform-up-and-running"
+    engine = "mysql"
+    allocated_storage = 10
+    instance_class = "db.t2.micro"
+    skip_final_snapshot = true
+    db_name = "example_database"
+
+    username = var.db_username
+    password = var.db_username
+  
+}
---
+++ stage/services/webserver-cluster/outputs.tf
@@ -0,0 +1,4 @@
+output "public_ip" {
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
+}
