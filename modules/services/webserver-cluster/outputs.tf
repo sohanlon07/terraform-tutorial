e8ca9424c32cc50c2d27ba951ea75f2c097cb4ec
Shane <sohanlon07@gmail.com>
2023-02-07 10:52:19 +0000

    Feature: Autoscaliong schedules
    Added autoscaling schedules to production for morning and evening
    Added output variables to module to get the output of the ASG to pass to
    prod web-server

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -2,3 +2,13 @@ output "public_ip" {
     value = aws_lb.example.dns_name
     description = "The domain name of the load balancer"
 }
+
+output "asg_name" {
+    value = aws_autoscaling_group.example.name
+    description = "The name of the Auto Scaling Group"
+}
+
+output "alb_dns_name" {
+    value = module.webserver_cluster.alb_dns_name
+    ddescription = "The domain name of the load balancer"  
+}
