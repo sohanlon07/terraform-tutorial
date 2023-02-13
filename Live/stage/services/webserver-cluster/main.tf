5f87a541d60272b3a6cec4e6cfad139bc5b678bd
Shane <sohanlon07@gmail.com>
2023-02-13 17:27:26 +0000

    Feature: Auotscaling to module
    Added variables to use autoscaling in modules

--- Live/stage/services/webserver-cluster/main.tf
+++ Live/stage/services/webserver-cluster/main.tf
@@ -24,11 +24,6 @@ module "webserver_cluster" {
     min_size = 2
     max_size = 2
     enable_autoscaling = false
-
-    # custom_tags {
-    #   Owner = "team-foo"
-    #   ManagedBy = "terraform"
-    # }
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
