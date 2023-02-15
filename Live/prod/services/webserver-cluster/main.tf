cb71ae4b76c724cc793280fea42f9a1e8d5db458
Shane <sohanlon07@gmail.com>
2023-02-15 09:33:02 +0000

    fix: add in custom tags which were removed

--- Live/prod/services/webserver-cluster/main.tf
+++ Live/prod/services/webserver-cluster/main.tf
@@ -13,6 +13,9 @@ terraform {
 module "webserver_cluster" {
     source = "git::https://gitlab.com/sohanlon07/terraform-tutorial-modules.git//modules/services/webserver-cluster?ref=main" 
 
+    ami = "ami-0fb653ca2d3203ac1"
+    server_text = "New Server Text PROD"
+
     cluster_name = "webservers-prod"
     db_remote_state_bucket = "terraform-state-file-storage-sohan"
     db_remote_state_key = "prod/datastores/mysql/terraform.tfstate"
@@ -21,7 +24,6 @@ module "webserver_cluster" {
     min_size = 2
     max_size = 3
     enable_autoscaling = true
-
     tags {
        Owner = "team-foo"
        ManagedBy = "terraform"
