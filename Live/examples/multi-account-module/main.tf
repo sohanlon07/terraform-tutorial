c071a3c306e9f6ef8fec33b44e887c0fcefb9c2e
Shane <sohanlon07@gmail.com>
2023-02-16 17:27:14 +0000

    fix: typos

---
+++ Live/examples/multi-account-module/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    region = "us-east-2"  
+    alias = "parent"
+}
+
+provider "aws" {
+    region = "us-east-2"  
+    alias = "child"
+
+    assume_role {
+      role_arn = "arn:aws:iam::953427527558:role/OrganizationAccountAccessRole"
+    }
+}
+
+module "multi_account_example" {
+    source =   "git::https://gitlab.com/sohanlon07/terraform-tutorial-modules.git//modules/examples/multi-account-root?ref=main" 
+
+    providers = {
+      aws.parent = aws.parent
+      aws.child = aws.child
+     }
+}
