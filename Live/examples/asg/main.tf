ac3245aa34fd1ca1bb611ec5b0ab1a5e3e4e173f
Shane <sohanlon07@gmail.com>
2023-02-21 15:28:56 +0000

    feat: created test harness to test asg modules
    Added a test harness to test asg and alb modules.

---
+++ Live/examples/asg/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "asg" {
+    source = "git::https://gitlab.com/sohanlon07/terraform-tutorial-modules.git//modules/cluster/asg-rolling-deploy?ref=main" 
+
+    cluster_name = var.cluster_name
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t2.micro"
+
+    min_size = 1
+    max_size = 2
+    enable_autoscaling = true
+
+    subnet_ids = data.aws_subnets.default.ids
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-terraform-tutorial-shane
+# Terraform Tutorials
+
+## Manual Test Harness
+
+Can use this example code while working on the asg-rolling-deploy module to repeatedly deploy it by manually running terraform apply and destroy to check it works as you expect.
+
+## Automated Test Harness
+
+This example code is also how you create automated tests for your modules.
