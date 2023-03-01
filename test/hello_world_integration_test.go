3b192fb1010c328e3c404a1df36b3418ddffada2
Shane <sohanlon07@gmail.com>
2023-03-01 16:18:24 +0000

    feat: added test stages for hello world app

--- test/hello_world_integration_test.go
+++ test/hello_world_integration_test.go
@@ -9,6 +9,7 @@ import (
 	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 const dbDirStage = "../live/stage/data-stores/mysql"
@@ -85,4 +86,56 @@ func validateHelloApp(t *testing.T, helloOpts *terraform.Options) {
 				strings.Contains(body, "Hello, World")
 		},
 	)
+}
+
+func TestHelloWorldAppStageWithStages(t *testing.T) {
+	t.Parallel()
+
+	//Store function in short var name to make code fit better
+	stage := test_structure.RunTestStage
+
+	//deploy mysql db
+	defer stage(t, "teardown_db", func() { tearDownDb(t, dbDirStage) })
+	stage(t, "deploy_db", func() { deployDB(t, dbDirStage) })
+
+	//Deploy HelloWorldApp
+	defer stage(t, "teardown_app", func() { tearDownApp(t, appDirStage) })
+	stage(t, "deploy_app", func() { deployApp(t, dbDirStage, appDirStage) })
+
+	//Validate helloworld app works
+	stage(t, "validate_app", func() { validateApp(t, appDirStage) })
+}
+
+func deployDB(t *testing.T, dbDir string) {
+	dbOpts := createDbOpts(t, dbDir)
+
+	//save data to disk so outher test stages can read data back in
+	test_structure.SaveTerraformOptions(t, dbDir, dbOpts)
+
+	terraform.InitAndApply(t, dbOpts)
+}
+
+func tearDownDb(t *testing.T, dbDir string) {
+	dbOpts := test_structure.LoadTerraformOptions(t, dbDir)
+	defer terraform.Destroy(t, dbOpts)
+}
+
+func deployApp(t *testing.T, dbDir string, helloAppDir string) {
+	dbOpts := test_structure.LoadTerraformOptions(t, dbDir)
+	helloOpts := createHelloOpts(dbOpts, helloAppDir)
+
+	//Save data to disk
+	test_structure.SaveTerraformOptions(t, helloAppDir, helloOpts)
+
+	terraform.InitAndApply(t, helloOpts)
+}
+
+func tearDownApp(t *testing.T, helloAppDir string) {
+	helloOpts := test_structure.LoadTerraformOptions(t, helloAppDir)
+	defer terraform.Destroy(t, helloOpts)
+}
+
+func validateApp(t *testing.T, helloAppDir string) {
+	helloOpts := test_structure.LoadTerraformOptions(t, helloAppDir)
+	validateHelloApp(t, helloOpts)
 }
